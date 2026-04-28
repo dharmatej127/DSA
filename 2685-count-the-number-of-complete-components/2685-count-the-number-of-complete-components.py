d07198e9e480c2d75d8935a6d07c07495ac736be
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-04-28 21:10:18 +0530

    Time: 27 ms (93.08%), Space: 19.6 MB (88.62%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,14 +1,29 @@
 class Solution:
-    def dfs(self,n,adj,vist,i):
+    # def dfs(self,n,adj,vist,i):
+        # vist[i]=True
+        # nodes=1
+        # edges=len(adj[i])
+        # for j in adj[i]:
+        #     if not vist[j]:
+        #         n,e=self.dfs(n,adj,vist,j)
+        #         nodes+=n
+        #         edges+=e
+        # return nodes,edges
+    def bfs(self,n,adj,vist,i):
+        q=[]
+        q.append(i)
         vist[i]=True
-        nodes=1
-        edges=len(adj[i])
-        for j in adj[i]:
-            if not vist[j]:
-                n,e=self.dfs(n,adj,vist,j)
-                nodes+=n
-                edges+=e
-        return nodes,edges
+        n=0
+        v=0
+        while q:
+            temp=q.pop(0)
+            n+=1
+            v+=len(adj[temp])
+            for i in adj[temp]:
+                if not vist[i]:
+                    vist[i]=True
+                    q.append(i)
+        return n,v//2
 
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
         adj=[]
@@ -21,8 +36,8 @@ class Solution:
         countCompleteCompo=0
         for i in range(n):
             if not vist[i]:
-                v,e=self.dfs(n,adj,vist,i)
-                e=e//2
+                v,e=self.bfs(n,adj,vist,i)
+               
                 if e==v*(v-1)//2:
                     countCompleteCompo+=1
         return countCompleteCompo
