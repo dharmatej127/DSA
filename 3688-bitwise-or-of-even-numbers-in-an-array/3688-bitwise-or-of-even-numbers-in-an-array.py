f0048872dd555476d6cb71d58850adae65a35b4b
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-07 22:05:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.44%) - LeetHub

---
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def evenNumberBitwiseORs(self, nums: List[int]) -> int:
+        res=0
+        for i in nums:
+            if i%2==0:
+                res|=i
+        return res
