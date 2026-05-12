f01c823e4201d943e64ac570b49d2c49083ae35d
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-12 20:51:27 +0530

    Time: 1 ms (17.2%), Space: 12.6 MB (48.35%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -11,24 +11,38 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        res=[]
-        def add(i,tree):
+        # res=[]
+        # def add(i,tree):
             
-            if len(res)<=i:
-                res.append([tree.val])
-            else:
-                res[i].append(tree.val)
-            i+=1
-            if tree.left:
-                add(i,tree.left)
-            if tree.right:
-                add(i,tree.right)
-        if root :
+        #     if len(res)<=i:
+        #         res.append([tree.val])
+        #     else:
+        #         res[i].append(tree.val)
+        #     i+=1
+        #     if tree.left:
+        #         add(i,tree.left)
+        #     if tree.right:
+        #         add(i,tree.right)
+        # if root :
                 
-            add(0,root)
+        #     add(0,root)
 
-        for i in range(len(res)):
-            if i%2!=0:
-                res[i].reverse()
+        # for i in range(len(res)):
+        #     if i%2!=0:
+        #         res[i].reverse()
+        # return res
+
+        res=[]
+        def traverse_count(node,depth=0):
+            if not node:
+                return 
+            if depth>=len(res):
+                res.append([])
+            if depth%2==0:
+                res[depth].append(node.val)
+            else:
+                res[depth].insert(0,node.val)
+            traverse_count(node.left,depth+1)
+            traverse_count(node.right,depth+1)
+        traverse_count(root)
         return res
-
