56070599a9eec3bae7756a7b0adb805fd0ab759e
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-14 10:04:30 +0530

    Time: 16 ms (90.56%), Space: 13.2 MB (88.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,15 +3,15 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """ 
-        max_reach=0
-        for i in range(len(nums)):
-            if (i>max_reach):
-                return False
-                break
-            max_reach = max(max_reach, i + nums[i])
+        # """ 
+        # max_reach=0
+        # for i in range(len(nums)):
+        #     if (i>max_reach):
+        #         return False
+        #         break
+        #     max_reach = max(max_reach, i + nums[i])
             
-        return True
+        # return True
         # i=0
         # while i>len(nums):
         #     if i<=len(nums):
@@ -19,3 +19,11 @@ class Solution(object):
         #     if i<len(nums):
         #         return False
         # return True
+        traget=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=traget:
+                traget=i
+        if traget==0:
+            return True
+        else:
+            return False
