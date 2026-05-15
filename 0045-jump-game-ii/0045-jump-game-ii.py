b64a34c3abeced466ce9c52a0e03d11f9bde1835
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-15 16:02:16 +0530

    Time: 2920 ms (11.45%), Space: 20 MB (66.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,9 +1,20 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        j,f,ce=0,0,0
-        for i in range(len(nums)-1):
-            f=max(f,i+nums[i])
-            if i==ce:
-                j+=1
-                ce=f
-        return j
+        # j,f,ce=0,0,0
+        # for i in range(len(nums)-1):
+        #     f=max(f,i+nums[i])
+        #     if i==ce:
+        #         j+=1
+        #         ce=f
+        # return j
+        n=len(nums)
+        dp=[float('inf')]*n
+        dp[0]=0
+
+        for i in range(len(nums)):
+            for j in range(1,nums[i]+1):
+                if i+j<n:
+                    # dp[j]=min(dp[j],dp[i]+1)
+                    dp[i+j]=min(dp[i+j],dp[i]+1)
+        return dp[-1]
+
