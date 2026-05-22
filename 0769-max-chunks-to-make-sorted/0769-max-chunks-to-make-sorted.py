c58c9e19fbf1b7cde0174b90d9238ba641b553e2
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-22 10:48:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (47.61%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        max_chunk=0
-        count=0
-        for i in range(len(arr)):
-            max_chunk=max(max_chunk,arr[i])
-            if max_chunk==i:
-                count+=1
-        return count
+        # max_chunk=0
+        # count=0
+        # for i in range(len(arr)):
+        #     max_chunk=max(max_chunk,arr[i])
+        #     if max_chunk==i:
+        #         count+=1
+        # return count
+
+        st=[]
+        for x in arr:
+            mx=x
+            while st and st[-1]>x:
+                mx=max(mx,st.pop())
+            st.append(mx)
+        return len(st)
