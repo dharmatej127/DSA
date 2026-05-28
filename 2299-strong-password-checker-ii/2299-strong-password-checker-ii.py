d80898eb64789dd9e1ee79415120683ba661b504
Dharmatej Vanamamaly <dharmatej127@gmail.com>
2026-05-28 23:28:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.9%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
@@ -1,27 +1,35 @@
 class Solution:
     def strongPasswordCheckerII(self, password: str) -> bool:
-        if len(password)<8:
-            return False
-
-        for i in range(len(password) - 1):
-            if password[i]==password[i + 1]:
-                return False
 
         count_u=0
         count_l=0
         count_s=0
         count_d=0
-
         chars="!@#$%^&*()-+"
 
-        for i in password:
-            if i.isupper():
+        if len(password)<8:
+            return False
+
+
+        for i in range(len(password) - 1):
+            if password[i]==password[i + 1]:
+                return False
+            if password[i].isupper():
                 count_u+=1
-            if i.islower():
+            if password[i].islower():
                 count_l+=1
-            if i.isdigit():
+            if password[i].isdigit():
                 count_d+=1
-            if i in chars:
+            if password[i] in chars:
                 count_s+=1
+        if password[-1].isupper():
+            count_u+=1
+        if password[-1].islower():
+            count_l+=1
+        if password[-1].isdigit():
+            count_d+=1
+        if password[-1] in chars:
+            count_s+=1
+
 
         return count_u>0 and count_l>0 and count_s>0 and count_d>0
